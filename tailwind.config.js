9a371b5bd858aa6d90e5671ac58acdfca5a9eea7
mc_gee02 <gerhard@poppgerhard.at>
2024-02-06 12:12:52 +0100

    implementiert die erste Zeile des Headers

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+    ],
+    theme: {
+        container: {
+            center: true,
+            padding: '2rem',
+        },
+        fontFamily: {
+          sans: ['montserrat', 'Arial', 'sans-serif']
+        },
+        extend: {
+            colors: {
+                "logo-blue": "#10275B",
+                "logo-dark-blue": "#050B1A",
+                "logo-light-blue": "#005FAC",
+                "logo-yellow": "#ECD012",
+                "logo-brown": "#402508",
+            }
+        },
+    },
+    plugins: [],
+}
+
