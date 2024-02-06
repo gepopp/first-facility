3cedec239cc39d2a4622273ca4f0e6e06893dc65
mc_gee02 <gerhard@poppgerhard.at>
2024-02-06 09:08:37 +0100

    filament users

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -19,6 +19,7 @@ class UserResource extends Resource
 
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
 
+
     public static function form(Form $form): Form
     {
         return $form
